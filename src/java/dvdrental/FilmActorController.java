427a0fdeb285c795bcead5904bf3cb211134f023
W1Ndextor <straylinej@chc.edu>
2017-02-13 22:48:17 -0500

    Implemented the add film functionality; needs debugging.

--- src/java/dvdrental/FilmActorController.java
+++ src/java/dvdrental/FilmActorController.java
@@ -28,6 +28,16 @@ public class FilmActorController implements Serializable {
 
     //this is our helper class that uses hibernate to query the database
     FilmActorHelper helper;
+    
+    //these fields map to components in the film.xhtml
+    //they represent the actual values input and selected by the user
+    String title;
+    String description;
+    int actor;
+    String rating;
+    int category;
+    int language;
+    String response;
 
     /**
      * Creates a new instance of FilmActorController
@@ -54,8 +64,8 @@ public class FilmActorController implements Serializable {
     return categoryValues ;
 }
 
-public void setCategoryValues(DataModel catgeoryValues) {
-        this.categoryValues = catgeoryValues;
+public void setCategoryValues(DataModel categoryValues) {
+        this.categoryValues = categoryValues;
     }
 
     public DataModel getLanguageValues() {
@@ -67,6 +77,82 @@ public void setCategoryValues(DataModel catgeoryValues) {
 
     public void setLanguageValues(DataModel languageValues) {
         this.languageValues = languageValues;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getActor() {
+        return actor;
+    }
+
+    public void setActor(int actor) {
+        this.actor = actor;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public int getCategory() {
+        return category;
+    }
+
+    public void setCategory(int category) {
+        this.category = category;
+    }
+
+    public int getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(int language) {
+        this.language = language;
+    }
+
+    public String getResponse() {
+        
+        if(title != null && description != null){
+            Date date = new Date();
+            Timestamp timeStamp = new Timestamp(date.getTime());
+            
+            if (helper.insert(title, description, actor, category, language, rating, timeStamp) == 1){
+                title = null;
+                description = null;
+                response = "Film Added.";
+                return response;
+            } else {
+                title = null;
+                description = null;
+                response = "Film Not Added.";
+                return response;
+            }
+        } else {
+            response = " ";
+            return response;
+        }
+        
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
     }
     
     
